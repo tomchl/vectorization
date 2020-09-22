9530d0a0cf0246c3c615730a02b36af1267ba4c5
Tomas Chlup <t.chlup@quadient.com>
2020-09-22 12:26:39 +0200

    get deleted

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import http.server
 import socketserver
 
-
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow.compat.v1 as tf
+import numpy as np
+from scipy.spatial import distance
 #To make tf 2.0 compatible with tf1.0 code, we disable the tf2.0 functionalities
 tf.disable_eager_execution()
 
@@ -26,32 +27,50 @@ class PragraphProcessor():
         paragraph_embedding = self.session.run(self.embedded_text, feed_dict={self.text_input: [paragraph]})
         return paragraph_embedding
 
+    def get_similarity(self, paragraphs):
+        if(len(paragraphs) != 2):
+            return 0.0
+
+        vectors = []
+        for paragraph in paragraphs:
+            paragraph_embedding = self.session.run(self.embedded_text, feed_dict={self.text_input: [paragraph]})
+            vectors.append(paragraph_embedding)
+
+        return distance.cosine(vectors[0], vectors[1])
+
+
 globalPragraphProcessor = PragraphProcessor()
 
 class PragraphProcessorHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        content_len = int(self.headers.get('Content-Length'))
-        body = str(self.rfile.read(content_len),'utf-8')
-
-        vector = globalPragraphProcessor.get_vector(body)
-
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(''.join(str(d) for d in vector), 'utf-8'))
-        print("Vectorization request finished.", PORT)
-    
     def do_POST(self):
-        content_len = int(self.headers.get('Content-Length'))
-        body = str(self.rfile.read(content_len),'utf-8')
+        if(self.path == "/vector"):
+            content_len = int(self.headers.get('Content-Length'))
+            body = str(self.rfile.read(content_len),'utf-8')
 
-        vector = globalPragraphProcessor.get_vector(body)
+            vector = globalPragraphProcessor.get_vector(body)
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(''.join(str(d) for d in vector), 'utf-8'))
-        print("Vectorization request finished.", PORT)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(''.join(str(d) for d in vector), 'utf-8'))
+            print("Vectorization request finished.", PORT)
+        elif(self.path == "/cosine"):
+            content_len = int(self.headers.get('Content-Length'))
+            body = str(self.rfile.read(content_len),'utf-8').split("&&")
+
+            similarity = globalPragraphProcessor.get_similarity(body)
+
+            self.send_response(201)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(str(similarity), 'utf-8'))
+            print("Similarity request finished.", PORT)
+        else:
+            self.send_response(401)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("not supported endpoint", 'utf-8')
+            print("WrongEndpointCalled.", PORT)
 
     
 PORT = 1020
