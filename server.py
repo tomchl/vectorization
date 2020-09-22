2d01152807d7752fa3d3c257471edaca785db54d
Tomas Chlup <t.chlup@quadient.com>
2020-09-22 08:22:24 +0200

    post implemented

--- server.py
+++ server.py
@@ -35,7 +35,20 @@ class PragraphProcessorHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(''.join(str(d) for d in vector), 'utf-8'))
         print("Vectorization request finished.", PORT)
+    
+    def do_POST(self):
+        content_len = int(self.headers.get('Content-Length'))
+        body = str(self.rfile.read(content_len),'utf-8')
 
+        vector = globalPragraphProcessor.get_vector(body)
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(''.join(str(d) for d in vector), 'utf-8'))
+        print("Vectorization request finished.", PORT)
+
+    
 PORT = 1020
 
 with socketserver.TCPServer(("", PORT), PragraphProcessorHandler) as httpd:
